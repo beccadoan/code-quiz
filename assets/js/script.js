6875c5b27ce51fea2af5432d9a26b826c7726aab
Becca Doan <rebecca.doan@cirrus.com>
2022-06-02 23:38:59 -0500

    added some javascript functionality when clicking start quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,37 @@
-var mainContentEl = $("#main-content");
+var quizDate = {
+    questions: ["Question 1", "Question 2", "Question 3", "Question 4"],
+    answers: [["A1","A2","A3","A4"],["A1","A2","A3","A4"],["A1","A2","A3","A4"],["A1","A2","A3","A4"]],
+    correctAnswers: [0,4,1,2]
+}
 
-$(mainContentEl).on("click", function(){
-    console.log("clicked the main function")
+
+var startQuiz = function(){
+    //initialize to first question
+    var questionNumber = 0;
+    // remove start quiz button
+    $("#start-quiz").remove();
+
+    // remove paragraph element describing quiz
+    $("#quiz-description").remove();
+
+    // change text in Header
+    $("#question-here").text("Question 1").removeClass(".text-center")
+
+    var answerDivEl = $("<div></div>").addAttr("id", "answers-here");
+    var answerListGroupEl = $("<ol></ol>")
+    spanEl.text("My span el");
+    answerDivEl.append(spanEl);
+
+    $('#main-content').append(answerDivEl);
+
+
+
+}
+
+
+
+
+// when "start quiz" button is clicked
+$("#start-quiz").on("click", function(){
+    startQuiz();
 })
